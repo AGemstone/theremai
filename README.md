2462862dbfe7893d65db9af5ac4b1fcb67e3f8a6
Adrian Edelstein <maradedelstein@gmail.com>
2024-02-20 20:25:29 -0300

    Changed interface to Qt6

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
-# TheremAI
-A virtual theremin that uses hand detection to control sound
+# theremai
+A virtual theremin that uses hand detection to control sound, initially designed as a proof of concept to recycle an old webcam.
 
 ## Install
-Set up a Python environment, install **Mediapipe**, **Python Sounddevice** **OpenCV**, and **Numpy**. Lastly run **main.py**
+Set up a Python environment using ```venv```
 
-## How to play
-Since the detection method is based on palm detection, for now the point of reference is the base of the hand
-* Right hand controls the frequency, the note that plays depends on the distance between the rightmost edge of the screen to the center.
-* Left hand controls amplitude, the loudness is determined by how far the hand is located above the lower edge of the screen
+Install required packages with ```pip install -r requirements.txt```
+
+Run ```main.py```
+
+## Flags
+* *--old_hw*: Don't use this, strictly for my old webcam.
--- main.py
+++ main.py
@@ -1,89 +1,48 @@
-import cv2
-import numpy as np
-import mediapipe as mp
-import random
-import sounddevice as sd
-import time
+#!/usr/bin/python3
 import sys
 
+from PyQt6.QtCore import Qt, QSize, QThread, pyqtSignal
+from PyQt6.QtGui import QImage, QPixmap, QKeyEvent
+from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel, QVBoxLayout
+from PyQt6.QtWidgets import QWidget
+
+import cv2
+import numpy as np
+
+import mediapipe as mp
 from mediapipe import solutions as mp_solutions
-from mediapipe.framework.formats import landmark_pb2
 
-MARGIN = 10  # pixels
-FONT_SIZE = 1
-FONT_THICKNESS = 1
-HANDEDNESS_TEXT_COLOR = (88, 205, 54)  # vibrant green
+import sounddevice as sd
 
 
-def draw_landmarks_on_image(rgb_image, detection_result):
-    hand_landmarks_list = detection_result.hand_landmarks
-    handedness_list = detection_result.handedness
-    annotated_image = np.copy(rgb_image)
-
-    # Loop through the detected hands to visualize.
-    for idx in range(len(hand_landmarks_list)):
-        hand_landmarks = hand_landmarks_list[idx]
-        handedness = handedness_list[idx]
-
-        # Draw the hand landmarks.
-        hand_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
-        hand_landmarks_proto.landmark.extend([
-            landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in hand_landmarks
-        ])
-        mp_solutions.drawing_utils.draw_landmarks(
-            annotated_image,
-            hand_landmarks_proto,
-            mp_solutions.hands.HAND_CONNECTIONS,
-            mp_solutions.drawing_styles.get_default_hand_landmarks_style(),
-            mp_solutions.drawing_styles.get_default_hand_connections_style())
-
-        # Get the top left corner of the detected hand's bounding box.
-        height, width, _ = annotated_image.shape
-        x_coordinates = [landmark.x for landmark in hand_landmarks]
-        y_coordinates = [landmark.y for landmark in hand_landmarks]
-        text_x = int(min(x_coordinates) * width)
-        text_y = int(min(y_coordinates) * height) - MARGIN
-
-        # Draw handedness (left or right hand) on the image.
-        cv2.putText(annotated_image, f"{handedness[0].category_name}",
-                    (text_x, text_y), cv2.FONT_HERSHEY_DUPLEX,
-                    FONT_SIZE, HANDEDNESS_TEXT_COLOR, FONT_THICKNESS, cv2.LINE_AA)
-
-    return annotated_image
-
-
-contrast = 255
-brightness = 90
-
-cv2.imshow("img", np.zeros((320, 240)))
-cv2.createTrackbar('frequency', "img", 44000, 44000, lambda x:  None)
-# Setup camera
-cap = cv2.VideoCapture()
-cap.open(-1, apiPreference=cv2.CAP_V4L2)
-# Necessary for my old webcam
-cap.set(cv2.CAP_PROP_FPS, 12.5)
-# These are inferred, but left commented just in case
-# cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
-# cap.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
-# cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
+WINDOW_SIZE = (800, 600)
 
 # Setup Mediapipe's hand detector
 BaseOptions = mp.tasks.BaseOptions
+
 # For landmark indices
 HandLandmark = mp_solutions.hands.HandLandmark
+
 # For detection
 HandLandmarker = mp.tasks.vision.HandLandmarker
 HandLandmarkerOptions = mp.tasks.vision.HandLandmarkerOptions
 HandLandmarkerResult = mp.tasks.vision.HandLandmarkerResult
 VisionRunningMode = mp.tasks.vision.RunningMode
 
-glob = None
-hands_last_known = [None, None]
 
-sd.default.device = "pipewire"
-samplerate = sd.query_devices(None, 'output')['default_samplerate']
-x = np.arange(0, 2, 1/samplerate)
-sin_table = np.sin(np.pi * x)
+def hand_callback(result: HandLandmarkerResult, output_image: mp.Image, timestamp_ms: int):
+    if len(result.handedness) == 2:
+        detected_hands = result.handedness[0][0], result.handedness[1][0]
+        if detected_hands[0].category_name == detected_hands[1].category_name:
+            return
+        hand_indices = detected_hands[0].index, detected_hands[1].index
+        freq = 2 * \
+            ((0.5 -
+             result.hand_landmarks[hand_indices[0]][HandLandmark.WRIST].x))
+        wave.frequency = 440-440*(freq)
+        wave.amplitude = 0.2 * \
+            np.clip(
+                (1 - result.hand_landmarks[hand_indices[1]][HandLandmark.WRIST].y), 0, 1)
 
 
 class WaveManager:
@@ -112,8 +71,7 @@ class WaveManager:
 
 
 samplerate = sd.query_devices(None, 'output')['default_samplerate']
-wave = WaveManager()
-waves = [wave.triangle, wave.sin, wave.saw, wave.square, wave.noise]
+sin_table = np.sin(np.pi * np.arange(0, 2, 1 / samplerate))
 
 
 def sd_callback(outdata, frames, audio_time, status):
@@ -125,68 +83,99 @@ def sd_callback(outdata, frames, audio_time, status):
             sin_table[int(wave.phase) % len(sin_table)]
     wave.start_idx += frames
 
-sd_out = sd.OutputStream(channels=2, callback=sd_callback)
+
+class CamThread(QThread):
+    img_signal = pyqtSignal(QImage)
+    ThreadActive = None
+
+    def run(self):
+        self.ThreadActive = True
+        cap = cv2.VideoCapture()
+        cap.open(0)
+        old_hw = None
+        if "--old_hw" in sys.argv:
+            old_hw = True
+            cap.set(cv2.CAP_PROP_FPS, 12.5)
+
+        fps = cap.get(cv2.CAP_PROP_FPS)
+
+        with HandLandmarker.create_from_options(HandLandmarkerOptions(
+                base_options=BaseOptions(
+                    model_asset_path='hand_landmarker.task'),
+                running_mode=VisionRunningMode.LIVE_STREAM,
+                num_hands=2,
+                # min_tracking_confidence=0.01,
+                min_hand_detection_confidence=0.85,
+                result_callback=hand_callback)) as landmarker, \
+                sd.OutputStream(channels=2, callback=sd_callback):
+            frame_count = 0
+            while self.ThreadActive:
+
+                success, frame = cap.read()
+                # Optimization if we don't edit capture
+                frame.flags.writeable = False
+                if success:
+                    frame_count += 1
+                    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                    frame = cv2.flip(frame, 1)
+                    mp_img = mp.Image(image_format=mp.ImageFormat.SRGB,
+                                      data=frame)
+                    landmarker.detect_async(mp_img, int(1000*frame_count/fps))
+                    qt_fmt = QImage(
+                        frame, frame.shape[1], frame.shape[0], QImage.Format.Format_RGB888)
+                    qt_fmt = qt_fmt.scaled(*WINDOW_SIZE)
+                    self.img_signal.emit(qt_fmt)
+
+                else:
+                    print("Capture failed!")
+                    cap.release()
+                    self.stop()
+                    break
+
+    def stop(self):
+        self.ThreadActive = False
+        self.terminate()
 
 
-def hand_callback(result: HandLandmarkerResult, output_image: mp.Image, timestamp_ms: int):
-    global glob, frequency, amplitude
-    img = output_image.numpy_view()
-    # img = cv2.GaussianBlur(img, (3, 3), 0)
-    # _, img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-    # img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
-    glob = draw_landmarks_on_image(img, result)
-    if len(result.handedness) == 2:
-        detected_hands = result.handedness[0][0], result.handedness[1][0]
-        if detected_hands[0].category_name == detected_hands[1].category_name:
-            return
-        hand_indices = detected_hands[0].index,detected_hands[1].index
-        freq = 2 * \
-            ((0.5 -
-             result.hand_landmarks[hand_indices[0]][HandLandmark.WRIST].x))
-        wave.frequency = 440-440*(freq)
-        wave.amplitude = 0.2 * \
-            np.clip(
-                (1 - result.hand_landmarks[hand_indices[1]][HandLandmark.WRIST].y), 0, 1)
+wave = WaveManager()
 
-with HandLandmarker.create_from_options(HandLandmarkerOptions(
-        base_options=BaseOptions(model_asset_path='hand_landmarker.task'),
-        running_mode=VisionRunningMode.LIVE_STREAM,
-        num_hands=2,
-        min_tracking_confidence=0.1,
-        min_hand_detection_confidence=0.2,
-        result_callback=hand_callback)) as landmarker, \
-        sd.OutputStream(channels=2, callback=sd_callback, latency=0.05):
-    while cap.isOpened():
-        # contrast = cv2.getTrackbarPos('contrast', 'img')
-        # wave.frequency = cv2.getTrackbarPos('frequency', 'img')/100
-        success, img = cap.read()
-        # img.flags.writeable = False
-        # print(f"cap is {success}")
-        if success:
-            # img = cv2.GaussianBlur(img,(7,7),0)
-            # img = cv2.fastNlMeansDenoisingColored(img,None,10,10,3,10)
-            # bright_contrast = (contrast / 255 * img + brightness)
-            # img = np.clip(bright_contrast.astype(np.uint8), 0, 255)
-            img = cv2.resize(img, dsize=(640, 480))
-            # img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-            # img = cv2.GaussianBlur(img,(5,5),0)
-            # _,img = cv2.threshold(img,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-            mp_img = mp.Image(image_format=mp.ImageFormat.SRGB, data=img)
-            # print(dir(mp_img))
-            landmarker.detect_async(mp_img, int(
-                cap.get(cv2.CAP_PROP_POS_MSEC)))
-            # cv2.imshow(cv2.cvtColor(annotated_image, cv2.COLOR_RGB2BGR))
-            if glob is not None:
-                cv2.imshow("img", glob)
 
-            # if not None in hands_last_known:
-            #     print(hands_last_known)
-        else:
-            print("Capture failed!")
-            break
-        if cv2.waitKey(30) & 0xFF == ord("q"):
-            print("Quiting...")
-            break
+class MainWindow(QMainWindow):
 
-cap.release()
-cv2.destroyAllWindows()
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("TheremAI")
+        self.camera_thread = CamThread()
+        self.camera_thread.start()
+
+        camera_feed = QLabel()
+        self.camera_thread.img_signal.connect(
+            lambda img: camera_feed.setPixmap(QPixmap.fromImage(img)))
+
+        layout = QVBoxLayout()
+        layout.addWidget(camera_feed)
+
+        container = QWidget()
+        container.setLayout(layout)
+
+        self.setCentralWidget(container)
+        self.setMinimumSize(QSize(*WINDOW_SIZE))
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key.Key_Escape:
+            print("Exiting!")
+            self.camera_thread.stop()
+            QApplication.quit()
+
+
+def main():
+    print("Starting app, quit with escape key.")
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    app.exec()
+
+
+if __name__ == "__main__":
+    main()
